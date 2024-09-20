489ee2cc998ef4148a88bd9f148eed8fb10943d9
RamonArrazola <jarrazola@exsoinf.com>
2024-09-20 11:44:51 -0600

    pagina 3 Creada

--- src/app/pages/pagina-tres/pagina-tres.page.ts
+++ src/app/pages/pagina-tres/pagina-tres.page.ts
@@ -1,16 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { RangeCustomEvent, toastController } from '@ionic/core';
 
 @Component({
   selector: 'app-pagina-tres',
   templateUrl: './pagina-tres.page.html',
   styleUrls: ['./pagina-tres.page.scss'],
 })
-export class PaginaTresPage implements OnInit {
+export class PaginaTresPage {
 
-  constructor() { }
+  public buffer = 0.0; 
+  public progress = 0.0; 
+  public isToastOpen = false;
 
-  ngOnInit() {
-    console.log("PaginaTres cargada");
+  constructor(private toastController: ToastController) { }
+
+  pinFormatter(value: number) {
+    return `${value}%`;
   }
 
+  onIonChange(ev: Event) {
+    this.presentToast(ev);
+  }
+
+  async presentToast(ev: Event) {
+    const toast = await this.toastController.create({
+      message: 'Se movio la barra de progreso a un ' + (ev as RangeCustomEvent).detail.value + '%',
+      duration: 1500,
+      position: 'bottom',
+    });
+
+    await toast.present();
+  }
 }
